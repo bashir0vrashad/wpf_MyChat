eafdf2a9a5a9da304aee9e25d9559af95a44f084
bashir0vrashad <beshirovreshad777@gmail.com>
2023-10-23 22:10:24 +0400

    mychat

---
+++ WpfApp1_chat/Chat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1_chat
+{
+    public class Chat
+    {
+        public string Text { get; set; }
+        public DateTime Datetime {get;set;}
+       
+            public Chat(string text, DateTime dt)
+        {
+            Text = text;
+            Datetime = dt;
+        }
+       
+    }
+}
---
+++ WpfApp1_chat/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1_chat
+{
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<Chat> _chats;
+
+        public ObservableCollection<Chat> chats { get; set; }
+       
+         
+        public MainWindow()
+        {
+            InitializeComponent();
+            chats = new ObservableCollection<Chat>();
+            DataContext = this;
+        }
+       
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void send_button(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(text.Text)) 
+            {
+               
+                chats.Add(new Chat(text.Text,DateTime.Now));
+             
+            }
+            text.Text = "";
+        }
+      
+    }
+}
